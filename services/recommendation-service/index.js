d918c3747ff7de85f7b54e47597cba4b5cacd1ee
Jovana Sekulic <sekulic2805@gmail.com>
2026-06-18 15:07:28 +0200

    Implementiran monitoring sa Prometheus i Grafana

--- services/recommendation-service/index.js
+++ services/recommendation-service/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
+const promClient = require('prom-client');
 const dotenv = require('dotenv');
 const connectDB = require('./src/models/db');
 const recommendationRoutes = require('./src/routes/recommendationRoutes');
@@ -12,11 +13,36 @@ connectDB();
 
 const app = express();
 
+const collectDefaultMetrics = promClient.collectDefaultMetrics;
+collectDefaultMetrics();
+
+const httpRequestCounter = new promClient.Counter({
+  name: 'http_requests_total',
+  help: 'Ukupan broj HTTP zahteva',
+  labelNames: ['method', 'route', 'status']
+});
+
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.on('finish', () => {
+    httpRequestCounter.inc({ method: req.method, route: req.path, status: res.statusCode });
+  });
+  next();
+});
+
 app.use('/api/recommendations', recommendationRoutes);
+
+app.get('/health', (req, res) => {
+  res.json({ status: 'Recommendation Service is running' });
+});
+
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', promClient.register.contentType);
+  res.end(await promClient.register.metrics());
+});
 
 const PORT = process.env.PORT || 3004;
 
